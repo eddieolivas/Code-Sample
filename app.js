834fe2782197703639b5a321196ed50bbb782108
eddieospaghettio <eddie@chrysaliswebdevelopment.com>
2014-09-16 15:05:56 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ var n = 5;
 
 function isOdd(num) { return num % 2;}
 
+//Setup the baggage list with n*2 baggage spaces and n*2 extra space with alternating B/A values starting at index n*2
 function setupBaggage() {
     var baggage = [];
-    //Setup the baggage list with n*2 baggage spaces and n*2 extra space with alternating B/A values starting at index n*2
     var twoN = n * 2;
     for ( i = 0 ; i < twoN*2; i++) {
         if( i > twoN - 1) {
@@ -28,10 +28,10 @@ var baggageList = setupBaggage();
 console.log("Before: " + baggageList);
 
 function moveBaggage(list, old_index, new_index) {
-    nextToOldIndex = old_index + 1;
-    nextToNewIndex = new_index + 1;
+    nextToOld = old_index + 1;
+    nextToNew = new_index + 1;
     list[old_index] = list[new_index], list[new_index] = "";
-    list[nextToOldIndex] = list[nextToNewIndex], list[nextToNewIndex] = "";
+    list[nextToOld] = list[nextToNew], list[nextToNew] = "";
 }
 
 function sortBaggage(list) {
@@ -43,7 +43,7 @@ function sortBaggage(list) {
     moveBaggage(list, 12, 15);
     moveBaggage(list, 15, 9);
     moveBaggage(list, 9, 18);
-    console.log("After: " + list + " " + listLength + " " + startPoint);
+    console.log("After: " + list);
 }
 
 sortBaggage(baggageList);
