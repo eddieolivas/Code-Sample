8f7565d99c1ceed2e84a54e4c192c62cf803df47
eddieospaghettio <eddie@chrysaliswebdevelopment.com>
2014-09-16 13:54:11 -0700

    Update app.js
    Removed prototype move function, added my own moveBaggage function to move 2 indexes at a time.

--- app.js
+++ app.js
@@ -2,24 +2,6 @@ var n = 5;
 
 function isOdd(num) { return num % 2;}
 
-//Function for moving items from one array index to another
-Array.prototype.move = function (old_index, new_index) {
-    while (old_index < 0) {
-        old_index += this.length;
-    }
-    while (new_index < 0) {
-        new_index += this.length;
-    }
-    if (new_index >= this.length) {
-        var k = new_index - this.length;
-        while ((k--) + 1) {
-            this.push(undefined);
-        }
-    }
-    this.splice(new_index, 0, this.splice(old_index, 1)[0]);
-    return this; // for testing purposes
-};
-
 function setupBaggage() {
     var baggage = [];
     //Setup the baggage list with n*2 baggage spaces and n*2 extra space with alternating B/A values starting at index n*2
@@ -43,30 +25,25 @@ function setupBaggage() {
 var baggageList = setupBaggage();
 
 //console.log(baggageList[-8]);
-console.log(baggageList);
-//Testing out the answer for n = 5
+console.log("Before: " + baggageList);
+
+function moveBaggage(list, old_index, new_index) {
+    nextToOldIndex = old_index + 1;
+    nextToNewIndex = new_index + 1;
+    list[old_index] = list[new_index], list[new_index] = "";
+    list[nextToOldIndex] = list[nextToNewIndex], list[nextToNewIndex] = "";
+}
+
 function sortBaggage(list) {
-    list[8] = list[17];
-    list[17] = "";
-    list[9] = list[18];
-    list[18] = "";
-    list[17] = list[12];
-    list[12] = "";
-    list[18] = list[13];
-    list[13] = "";
-    list[12] = list[15];
-    list[15] = "";
-    list[13] = list[16];
-    list[16] = "";
-    list[15] = list[9];
-    list[9] = "";
-    list[16] = list[10];
-    list[10] = "";
-    list[9] = list[18];
-    list[18] = "";
-    list[10] = list[19];
-    list[19] = "";
-    console.log(list);
+    listLength = list.length;
+    startPoint = listLength / 2 - 1;
+    moveOne = listLength - 3;
+    moveBaggage(list, startPoint, moveOne);
+    moveBaggage(list, 17, 12);
+    moveBaggage(list, 12, 15);
+    moveBaggage(list, 15, 9);
+    moveBaggage(list, 9, 18);
+    console.log("After: " + list + " " + listLength + " " + startPoint);
 }
 
 sortBaggage(baggageList);
